5838e712015a027b855efb653ff49a3928f9ba06
PauloAlves8039 <pj38alves@gmail.com>
2023-10-31 14:46:04 -0300

    Criando solução para o desafio 07

---
+++ src/Desafio-07/js/script.js
@@ -0,0 +1,25 @@
+const paragraph = document.getElementById("paragraph");
+
+let fontColor = "#000000";
+let backgroundColor = "#FFFFFF";
+let currentSize = window.getComputedStyle(paragraph, null).getPropertyValue('font-size');
+
+function changeTextColor() {
+  fontColor = fontColor === "#000000" ? "#FFFFFF" : "#000000";
+  paragraph.style.color = fontColor;
+}
+
+function changeBackgroundColor() {
+  backgroundColor = backgroundColor === "#FFFFFF" ? "black" : "#FFFFFF";
+  paragraph.style.backgroundColor = backgroundColor;
+}
+
+function increaseFontSize() {
+  currentSize = (parseFloat(currentSize) + 1) + 'px';
+  paragraph.style.fontSize = currentSize;
+}
+
+function decreaseFontSize() {
+  currentSize = (parseFloat(currentSize) - 1) + 'px';
+  paragraph.style.fontSize = currentSize;
+}
